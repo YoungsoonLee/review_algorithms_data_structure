faee48d66aa58d99f58195012139ac5dfb1b0e6c
YoungsoonLee <youngtip@gmail.com>
2017-02-17 17:15:15 -0800

    add stack witk linkedlist

--- DS_stack_with_ll.py
+++ DS_stack_with_ll.py
@@ -21,8 +21,25 @@ class Stack():
 			while cur.next:
 				cur = cur.next			
 			cur.next = node
-			self.top = cur
 		self.count += 1
+
+	def pop(self):
+		cur = self.top
+		pre = None
+		while cur.next:
+			pre = cur
+			cur = cur.next
+		value = cur
+		pre.next = None
+		return value
+
+	def getTopIndex(self):
+		cur = self.top
+		index = 0
+		while cur:
+			cur = cur.next
+			index += 1
+		return index-1
 
 	def __iter__(self):
 		cur = self.top
@@ -36,3 +53,20 @@ class Stack():
 		# print(values)
 		return ' | '.join(values)
 
+
+if __name__ == "__main__":
+	st = Stack()
+	st.push(1)
+	st.push(2)
+	st.push(3)
+	st.push(4)
+	st.push(5)
+	st.push(6)
+	st.push(7)
+	print(st)
+	print(st.getTopIndex())
+	print(st.pop())
+	print(st.pop())
+	print(st.pop())
+	print(st)
+	
